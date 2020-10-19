dee3e8ecc63e165f1da6f1535862ca6a2cd722d4
BuildTools <kobe.albright@gmail.com>
2020-10-19 17:08:28 -0400

    Migrate to .NET 5

--- Valour/Server/Controllers/WeatherForecastController.cs
+++ Valour/Server/Controllers/WeatherForecastController.cs
@@ -1,10 +1,10 @@
-﻿using Valour.Shared;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Valour.Shared;
 
 namespace Valour.Server.Controllers
 {
@@ -17,11 +17,11 @@ namespace Valour.Server.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            this.logger = logger;
+            _logger = logger;
         }
 
         [HttpGet]
