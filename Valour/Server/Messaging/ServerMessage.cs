a51b1dbe4faf972e7dc02189252f939a6222ceb5
BuildTools <kobe.albright@gmail.com>
2020-10-26 19:24:20 -0400

    Server message (hash) setup

--- Valour/Server/Messaging/ServerMessage.cs
+++ Valour/Server/Messaging/ServerMessage.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Valour.Server.Models;
 
 namespace Valour.Server.Messaging
 {
     public class ServerMessage
     {
+        [Key]
+        public byte[] Hash { get; set; }
 
+        /// <summary>
+        /// Returns true if the client message matches this server message
+        /// </summary>
+        public bool EqualsMessage(ClientMessage message)
+        {
+            return (Hash == message.GetHash());
+        }
     }
 }
