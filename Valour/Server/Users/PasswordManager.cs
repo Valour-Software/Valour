2684fac5bfc547533126805b15786a382127e419
BuildTools <kobe.albright@gmail.com>
2020-12-14 15:01:58 -0500

    Simple server-side user fields

--- Valour/Server/Users/PasswordManager.cs
+++ Valour/Server/Users/PasswordManager.cs
@@ -7,19 +7,24 @@ using System.Threading.Tasks;
 
 namespace Valour.Shared.Users
 {
+    /// <summary>
+    /// This class handles password hashing and equality.
+    /// The goal is the triple S of Spikeness <^> (A concept that will be in business books by 2077):
+    /// Simplicity, Security, Scalability.
+    /// </summary>
     public static class PasswordManager
     {
         static readonly HashAlgorithmName HashName = HashAlgorithmName.SHA256;
         const int Iterations = 30000;
 
-        public static byte[] GetHashForPassword(string password, string salt)
+        public static byte[] GetHashForPassword(string password, byte[] salt)
         {
             byte[] passBytes = Encoding.Unicode.GetBytes(password);
-            byte[] saltBytes = Encoding.Unicode.GetBytes(salt);
 
-            using (var deriveBytes = new Rfc2898DeriveBytes(passBytes, saltBytes, Iterations, HashName))
+            using (var deriveBytes = new Rfc2898DeriveBytes(passBytes, salt, Iterations, HashName))
             {
-                return deriveBytes.GetBytes(128);
+                // Returns 128-bit hash
+                return deriveBytes.GetBytes(16);
             }
         }
     }
---
+++ Valour/Server/Users/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Valour.Shared.Users;
+
+namespace Valour.Server.Users
+{
+    /// <summary>
+    /// This is the server-side User implementation. It contains all content from the Client-side,
+    /// and includes all hidden fields needed for server functions.
+    /// </summary>
+    public class User : ClientUser
+    {
+        /// <summary>
+        /// The user password hash. This should NOT be able to be reached by the client,
+        /// and should be 16 bytes (128 bits)
+        /// </summary>
+        public byte[] Password_Hash { get; set; }
+
+        /// <summary>
+        /// The unique salt for the password.
+        /// Not to be confused with league of legends players.
+        /// </summary>
+        public byte[] Salt { get; set; }
+
+        /// <summary>
+        /// Will return true if the password is correct for the user
+        /// </summary>
+        public bool VerifyPassword(string password)
+        {
+            return PasswordManager.GetHashForPassword(password, Salt) == Password_Hash;
+        }
+    }
+}
