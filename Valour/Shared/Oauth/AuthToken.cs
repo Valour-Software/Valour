a32482aa2e75b638147ca31843e0ea54a43a1f85
BuildTools <kobe.albright@gmail.com>
2020-12-15 14:25:38 -0500

    Add Oauth token class

---
+++ Valour/Shared/Oauth/AuthToken.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valour.Shared.Oauth
+{
+    /*  Valour - A free and secure chat client
+    *  Copyright (C) 2020 Vooper Media LLC
+    *  This program is subject to the GNU Affero General Public license
+    *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+    */
+
+    public class AuthToken
+    {
+        /// <summary>
+        /// The ID of the authentification key is also the secret key. Really no need for another random gen.
+        /// (is sha256)
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The ID of the app that has been issued this token
+        /// </summary>
+        public string App_Id { get; set; }
+
+        /// <summary>
+        /// The user that this token is valid for
+        /// </summary>
+        public string User_Id { get; set; }
+
+        /// <summary>
+        /// The scope of the permissions this token is valid for
+        /// </summary>
+        public ulong Scope { get; set; }
+
+        /// <summary>
+        /// The time that this token was issued
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        /// <summary>
+        /// The time that this token will expire
+        /// </summary>
+        public DateTime Expires { get; set; }
+    }
+}
