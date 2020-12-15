dde8aa9d5a5450bb359861f8e7c4bb990ad3cfe1
BuildTools <kobe.albright@gmail.com>
2020-12-15 16:21:19 -0500

    Fix jacob crying

--- Valour/Server/Users/UserManager.cs
+++ Valour/Server/Users/UserManager.cs
@@ -1,19 +1,46 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Valour.Server.Database;
+using Valour.Shared;
 using Valour.Shared.Users;
 
 namespace Valour.Server.Users
 {
     public class UserManager
     {
+
+        /// <summary>
+        /// The result from a user validation attempt
+        /// </summary>
+        public class ValidateResult
+        {
+            /// <summary>
+            /// The result for the validation
+            /// </summary>
+            public TaskResult Result { get; set; }
+
+            /// <summary>
+            /// The user retrieved (if any)
+            /// </summary>
+            public User User { get; set; }
+
+            public ValidateResult(TaskResult result, User user)
+            {
+                this.Result = result;
+                this.User = user;
+            }
+        }
+
         /// <summary>
         /// Validates and returns a User using credentials (async)
         /// </summary>
-        public async Task<User> ValidateAsync(string credential_type, string identifier, string secret)
+        public async Task<ValidateResult> ValidateAsync(string credential_type, string identifier, string secret)
         {
             using (ValourDB context  = new ValourDB(ValourDB.DBOptions))
             {
@@ -27,19 +54,38 @@ namespace Valour.Server.Users
 
                 if (hash != credential.Secret)
                 {
-                    return null;
+                    return new ValidateResult(new TaskResult(false, "Secret validation failed."), null);
                 }
 
-                return await context.Users.FindAsync(credential.User_Id);
+                User user = await context.Users.FindAsync(credential.User_Id);
+
+                return new ValidateResult(new TaskResult(true, "Succeeded"), user);
             }
         }
 
         /// <summary>
         /// Validates and returns a User using credentials
         /// </summary>
-        public User Validate(string credential_type, string identifier, string secret)
+        public ValidateResult Validate(string credential_type, string identifier, string secret)
         {
             return ValidateAsync(credential_type, identifier, secret).Result;
+        }
+
+        /// <summary>
+        /// Logs the user in 
+        /// </summary>
+        public async Task LogInUser(HttpContext http, User user, bool isPersistant = false)
+        {
+            ClaimsIdentity identity = new ClaimsIdentity();
+        }
+
+        /// <summary>
+        /// Returns the role claims for a given user
+        /// </summary>
+        private IEnumerable<Claim> GetUserRoleClaims(User user)
+        {
+            List<Claim> claims = new List<Claim>();
+            IEnumerable<int> roleIds 
         }
     }
 }
