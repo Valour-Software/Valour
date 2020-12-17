b9d57cdb1928b8ddfce03edc02bcc294e4f4104b
BuildTools <kobe.albright@gmail.com>
2020-12-17 14:40:17 -0500

    Fix page layouts and add register razor

---
+++ Valour/Client/wwwroot/js/loading.js
@@ -0,0 +1,13 @@
+﻿var a = 10,
+    b = 250,
+    c = 255;
+
+setInterval(function () {
+    255 >= a && 40 == b && 40 == c ? a++ : a;
+    255 == a && 40 == c && 255 >= b ? b++ : b;
+    255 == a && 255 == b && 255 >= c ? c++ : c;
+    255 == c && 255 == b && 10 < a ? a-- : a;
+    10 == a && 255 == c && 40 < b ? b-- : b;
+    10 == a && 40 == b && 40 < c ? c-- : c;
+    document.querySelector('.d').style.color = 'rgb(' + a + ',' + b + ',' + c + ')'
+}, 5);
