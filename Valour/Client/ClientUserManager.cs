522411417374ca2f84774c845222a147852fff7e
BuildTools <kobe.albright@gmail.com>
2020-12-17 13:52:36 -0500

    Login persistance and local storage

--- Valour/Client/ClientUserManager.cs
+++ Valour/Client/ClientUserManager.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -7,6 +8,9 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Valour.Shared;
 using Valour.Shared.Users;
+using Valour.Shared.Oauth;
+using System.Security.Cryptography;
+using Blazored.LocalStorage;
 
 namespace Valour.Client
 {
@@ -41,10 +45,30 @@ namespace Valour.Client
         }
 
         /// <summary>
+        /// Tries to initialize the user client by using a local token
+        /// </summary>
+        public static async Task<TaskResult> TryInitializeWithLocalToken(HttpClient http, LocalStorageService storage)
+        {
+            if (User != null)
+            {
+                return new TaskResult(false, "There's already a user logged in!");
+            }
+
+            await RetrieveToken(storage);
+
+            // Cancel if no token is found
+            if (UserSecretToken == null || string.IsNullOrWhiteSpace(UserSecretToken))
+            {
+                return new TaskResult(false, "Failed to retrieve local token.");
+            }
+
+            return  await InitializeUser(UserSecretToken, http, storage);
+        }
+
+        /// <summary>
         /// Initializes the user using a valid user token
         /// </summary>
-        /// <param name="token"></param>
-        public static async Task<TaskResult> InitializeUser(string token, HttpClient http)
+        public static async Task<TaskResult> InitializeUser(string token, HttpClient http, LocalStorageService storage)
         {
             string response = await http.GetStringAsync($"User/GetUserWithToken?token={token}");
 
@@ -55,10 +79,43 @@ namespace Valour.Client
                 User = result.Data;
                 UserSecretToken = token;
 
+                Console.WriteLine($"Initialized user {User.Username}");
+
+                // Store token for future use
+                await StoreToken(storage);
+
                 return new TaskResult(true, "Initialized user successfully!");
             }
 
             return new TaskResult(false, "An error occured retrieving the user.");
+        }
+
+        /// <summary>
+        /// Stores the current user token
+        /// </summary>
+        public static async Task StoreToken(LocalStorageService storage)
+        {
+            LocalToken tokenObj = new LocalToken()
+            {
+                Token = UserSecretToken
+            };
+
+            await storage.SetItemAsync("token", tokenObj);
+
+            Console.WriteLine("Stored user token in local storage.");
+        }
+
+        /// <summary>
+        /// Attempts to retrieve a prior user token
+        /// </summary>
+        public static async Task RetrieveToken(LocalStorageService storage)
+        {
+            LocalToken tokenObj = await storage.GetItemAsync<LocalToken>("token");
+
+            if (tokenObj != null)
+            {
+                UserSecretToken = tokenObj.Token;
+            }
         }
     }
 }
