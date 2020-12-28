7229a25c27847a8d881214d9544576ba24a89958
BuildTools <kobe.albright@gmail.com>
2020-12-28 15:15:24 -0500

    Planet User membership object

---
+++ Valour/Shared/Planets/PlanetUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valour.Shared.Planets
+{
+    /*  Valour - A free and secure chat client
+     *  Copyright (C) 2020 Vooper Media LLC
+     *  This program is subject to the GNU Affero General Public license
+     *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+     */
+
+
+    /// <summary>
+    /// This represents a user within a planet
+    /// </summary>
+    class PlanetUser
+    {
+        /// <summary>
+        /// The user within the planet
+        /// </summary>
+        public ulong User_Id { get; set; }
+
+        /// <summary>
+        /// The planet the user is within
+        /// </summary>
+        public ulong Planet_Id { get; set; }
+    }
+}
--- Valour/Shared/Users/ClientUser.cs
+++ Valour/Shared/Users/ClientUser.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Valour.Shared.Planets;
 using Valour.Shared.Users;
 
 namespace Valour.Shared.Users
 {
+    /*  Valour - A free and secure chat client
+     *  Copyright (C) 2020 Vooper Media LLC
+     *  This program is subject to the GNU Affero General Public license
+     *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+     */
+
     /// <summary>
     /// This is the private User implementation, which should only be held by the server and local client.
     /// </summary>
@@ -21,5 +28,15 @@ namespace Valour.Shared.Users
         /// The user's email address
         /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// Cache for joined planet objects
+        /// </summary>
+        public List<Planet> Planets = new List<Planet>();
+
+        public async Task RefreshPlanets()
+        {
+
+        }
     }
 }
