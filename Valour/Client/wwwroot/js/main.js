c3af4f295d8e97b1253cf7f7d7a1159e4765e6f1
BuildTools <kobe.albright@gmail.com>
2021-01-07 21:33:49 -0500

    Style improvements

--- Valour/Client/wwwroot/js/main.js
+++ Valour/Client/wwwroot/js/main.js
@@ -6,14 +6,41 @@ function SizeEnable() {
     var man = $('#window-man');
 
     if (man.children().length > 1) {
+        var split = Split(
+            man.children(),
+            {
+                minSize: [300, 300],
+                gutterAlign: 'center',
+                gutterSize: 3,
+            }
+        );
 
+        var col1 = $('#window-col1');
 
-            var split = Split(
-                man.children(),
+        if (col1.children().length > 1) {
+            split = Split(
+                col1.children(),
                 {
-                    minSize: [300, 300]
+                    minSize: [300, 300],
+                    direction: 'vertical',
+                    gutterAlign: 'center',
+                    gutterSize: 3,
                 }
             );
-        
+        }
+
+        var col2 = $('#window-col2');
+
+        if (col2.children().length > 1) {
+            split = Split(
+                col2.children(),
+                {
+                    minSize: [300, 300],
+                    direction: 'vertical',
+                    gutterAlign: 'center',
+                    gutterSize: 3,
+                }
+            );
+        }
     }
 }
