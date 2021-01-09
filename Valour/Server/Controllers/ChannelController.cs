082b0738680309b9c088067e3361d0b956811af5
superjacobl <jacoblower26@gmail.com>
2021-01-09 12:20:46 -0500

    Added Channel Creation

--- Valour/Server/Controllers/ChannelController.cs
+++ Valour/Server/Controllers/ChannelController.cs
@@ -10,6 +10,9 @@ using Valour.Server.Messaging;
 using Valour.Shared;
 using Valour.Shared.Channels;
 using Valour.Shared.Messages;
+using Valour.Shared.Oauth;
+using Valour.Shared.Planets;
+using System.Text.RegularExpressions;
 
 /*  Valour - A free and secure chat client
  *  Copyright (C) 2020 Vooper Media LLC
@@ -38,6 +41,76 @@ namespace Valour.Server.Controllers
         public ChannelController(ValourDB context)
         {
             this.Context = context;
+        }
+        
+        /// <summary>
+        /// Creates a server and if successful returns a task result with the created
+        /// planet's id
+        /// </summary>
+        public async Task<TaskResult<ulong>> CreateChannel(string name, ulong userid, string token, string planetid)
+        {
+            TaskResult nameValid = ValidateName(name);
+
+            if (!nameValid.Success)
+            {
+                return new TaskResult<ulong>(false, nameValid.Message, 0);
+            }
+
+            AuthToken authToken = await Context.AuthTokens.FindAsync(token);
+
+            // Return the same if the token is for the wrong user to prevent someone
+            // from knowing if they cracked another user's token. This is basically 
+            // impossible to happen by chance but better safe than sorry in the case that
+            // the literal impossible odds occur, more likely someone gets a stolen token
+            // but is not aware of the owner but I'll shut up now - Spike
+            if (authToken == null || authToken.User_Id != userid)
+            {
+                return new TaskResult<ulong>(false, "Failed to authorize user.", 0);
+            }
+
+            // User is verified and given channel info is valid by this point
+
+            // Converts planetid into a ulong
+            
+            ulong planetId = Convert.ToUInt64(planetid);
+
+            // Creates the channel channel
+
+            PlanetChatChannel channel = new PlanetChatChannel()
+            {
+                Name = name,
+                Planet_Id = planetId,
+                Message_Count = 0
+            };
+
+            // Add channel to database
+            await Context.PlanetChatChannels.AddAsync(channel);
+
+            // Save changes to DB
+            await Context.SaveChangesAsync();
+
+            // Return success
+            return new TaskResult<ulong>(true, "Successfully created channel.", channel.Id);
+        }
+
+        public Regex planetRegex = new Regex(@"^[a-zA-Z0-9 _-]+$");
+
+        /// <summary>
+        /// Validates that a given name is allowable for a server
+        /// </summary>
+        public TaskResult ValidateName(string name)
+        {
+            if (name.Length > 32)
+            {
+                return new TaskResult(false, "Planet names must be 32 characters or less.");
+            }
+
+            if (!planetRegex.IsMatch(name))
+            {
+                return new TaskResult(false, "Planet names may only include letters, numbers, dashes, and underscores.");
+            }
+
+            return new TaskResult(true, "The given name is valid.");
         }
 
         [HttpGet]
