9e4abb4600bb3ead9e1feb1cb0f675c719d0d36f
superjacobl <jacoblower26@gmail.com>
2021-01-13 21:32:07 -0500

    Added model for 24h Cached Messages

---
+++ Valour/Server/Messaging/CacheMessage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Valour.Shared.Messages;
+using System.Security.Cryptography;
+using System.Text;
+
+/*  Valour - A free and secure chat client
+ *  Copyright (C) 2020 Vooper Media LLC
+ *  This program is subject to the GNU Affero General Public license
+ *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace Valour.Server.Messaging
+{
+    public class CacheMessage
+    {
+        [Key]
+        public ulong Id { get; set; }
+
+        /// <summary>
+        /// The user's ID, which is a GUID
+        /// </summary>
+        public ulong Author_Id { get; set; }
+
+        /// <summary>
+        /// String representation of message
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// The time the message was sent (in UTC)
+        /// </summary>
+        public DateTime TimeSent { get; set; }
+
+        /// <summary>
+        /// Id of the channel this message belonged to
+        /// </summary>
+        public ulong ChannelId { get; set; }
+
+        /// <summary>
+        /// Index of the message
+        /// </summary>
+        public ulong Index { get; set; }
+
+        /// <summary>
+        /// Returns the hash for a message. Cannot be used in browser/client!
+        /// </summary>
+        public byte[] GetHash()
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                string conc = $"{Author_Id}{Content}{TimeSent}{ChannelId}";
+
+                byte[] buffer = Encoding.Unicode.GetBytes(conc);
+
+                return sha.ComputeHash(buffer);
+            }
+        }
+    }
+}
