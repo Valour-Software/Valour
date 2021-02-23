defb04d480b9f624b389bc35dd85740b81228075
BuildTools <kobe.albright@gmail.com>
2021-02-23 10:06:27 -0500

    Role creation work

--- Valour/Client/wwwroot/js/main.js
+++ Valour/Client/wwwroot/js/main.js
@@ -718,6 +718,53 @@ window.blazorFuncs = {
     }
 };
 
+function SetupColorPicker() {
+
+    console.log("Setting up color picker...");
+
+    const pickr = Pickr.create({
+        el: '.color-picker',
+        theme: 'nano', // or 'monolith', or 'nano'
+
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)'
+        ],
+
+        components: {
+
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+
+            // Input / output Options
+            interaction: {
+                hex: true,
+                rgba: true,
+                hsla: true,
+                hsva: true,
+                cmyk: true,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+}
+
 window.onresize = function () {
     document.body.height = window.innerHeight;
 }
