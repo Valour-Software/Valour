c4ea43b84c228c99efa9e183be05390f72544594
Kobe Albright <kobe.albright@gmail.com>
2021-11-16 00:09:11 -0600

    Image uploads

--- Valour/Server/API/UploadAPI.cs
+++ Valour/Server/API/UploadAPI.cs
@@ -36,18 +36,96 @@ namespace Valour.Server.API
         {
             app.MapPost("/upload/profileimage", ProfileImageRoute);
             app.MapPost("/upload/planetimage", PlanetImageRoute);
+            app.MapPost("/upload/image", ImageRoute);
         }
 
-        private static async Task ProfileImageRoute(HttpContext context, HttpClient http, ValourDB db)
-        {
-            if (!context.Request.Query.TryGetValue("auth", out var auth))
+
+        // 10240000
+        private static async Task ImageRoute(HttpContext context, HttpClient http, ValourDB db, 
+            [FromHeader] string authorization){
+
+            var authToken = await ServerAuthToken.TryAuthorize(authorization, db);
+
+            if (authToken == null)
             {
                 context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("Unauthorized. Include auth.");
+                await context.Response.WriteAsync("Unauthorized.");
                 return;
             }
 
-            var authToken = await ServerAuthToken.TryAuthorize(auth, db);
+            if (context.Request.ContentLength < 512)
+            {
+                context.Response.StatusCode = 413;
+                await context.Response.WriteAsync("Must be greater than 512 bytes");
+                return;
+            }
+
+            // Max file size is 10mb
+            if (context.Request.ContentLength > 10240000)
+            {
+                context.Response.StatusCode = 413;
+                await context.Response.WriteAsync("Max file size is 10mb");
+                return;
+            }
+
+            int fileCount = context.Request.Form.Files.Count;
+
+            Console.WriteLine("Count: " + fileCount);
+
+            if (fileCount == 0)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("Please attach an image");
+                return;
+            }
+
+            if (fileCount > 1)
+            {
+                context.Response.StatusCode = 413;
+                await context.Response.WriteAsync("Please attach one image only");
+                return;
+            }
+
+            var file = context.Request.Form.Files[0];
+
+            if (file.Length > 10240000)
+            {
+                context.Response.StatusCode = 413;
+                await context.Response.WriteAsync("Max total size is 10mb");
+                return;
+            }
+
+            // Ensure it conforms to limits
+
+            if (!MPSManager.Image_Types.Contains(file.ContentType))
+            {
+                context.Response.StatusCode = 415;
+                await context.Response.WriteAsync("Ensure file is an image.");
+                return;
+            }
+
+            // Forward to VMPS
+
+            byte[] data = new byte[file.Length];
+
+            await file.OpenReadStream().ReadAsync(data);
+
+            var content = new MultipartFormDataContent();
+            var arrContent = new ByteArrayContent(data);
+            arrContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+            content.Add(arrContent, "file", file.Name);
+
+            var response = await http.PostAsync($"https://vmps.valour.gg/Upload/Image?auth={MPSConfig.Current.Api_Key_Encoded}", content);
+
+            context.Response.StatusCode = (int)response.StatusCode;
+            await response.Content.CopyToAsync(context.Response.BodyWriter.AsStream());
+        }
+
+        private static async Task ProfileImageRoute(HttpContext context, HttpClient http, ValourDB db, 
+            [FromHeader] string authorization)
+        {
+            var authToken = await ServerAuthToken.TryAuthorize(authorization, db);
 
             if (authToken == null)
             {
