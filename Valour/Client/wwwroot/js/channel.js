3fbe5df7ab7ea770540778b07ad800bfd663b597
Jacob <jacoblower4@gmail.com>
2022-01-18 16:07:08 -0500

    pasting test now keeps the newlines

--- Valour/Client/wwwroot/js/channel.js
+++ Valour/Client/wwwroot/js/channel.js
@@ -129,6 +129,8 @@ function OnChatboxUpdate(e, box) {
 function OnChatboxPaste(e, box) {
     e.preventDefault();
     var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+    // we need to put the pasted text in a span to keep the newlines
+    text = `<span style="white-space: pre;">`+text+`</span>`
     document.execCommand("insertHTML", false, text);
 }
 
