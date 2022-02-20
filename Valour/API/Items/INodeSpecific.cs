2bc7257df28e1610748815e9cd214c857ab1844c
Kobe Albright <kobe.albright@gmail.com>
2022-02-20 16:05:11 -0600

    Working on node API - just figured out a big idea

---
+++ Valour/API/Items/INodeSpecific.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Valour.Database.Items;
+
+/// <summary>
+/// This interface specifies that an item is designed to be returned from
+/// a specific node
+/// </summary>
+public interface INodeSpecific
+{
+    /// <summary>
+    /// This is the node that returned the API item.
+    /// This node should be used for any API 
+    /// </summary>
+    [NotMapped]
+    [JsonInclude]
+    [JsonPropertyName("Node")]
+    public string Node { get; set; }
+}
