adeee97cc5cd4d68b14026d38a763d2a4c2dcdf9
superjacobl <jacoblower4@gmail.com>
2022-07-12 16:23:30 -0400

    made embed item name's less white; fixed wide embeds; added titles & footers

--- Valour/Shared/Items/Messages/Embeds/EmbedBuilder.cs
+++ Valour/Shared/Items/Messages/Embeds/EmbedBuilder.cs
@@ -4,7 +4,7 @@ public class EmbedPageBuilder
 {
     public List<EmbedItem> Items = new();
 
-    public EmbedPageBuilder AddText(string name = "", string text = "", bool inline = false, string textColor = "ffffff")
+    public EmbedPageBuilder AddText(string name = "", string text = "", bool inline = false, string textColor = "eeeeee")
     {
         EmbedItem item = new()
         {
@@ -18,7 +18,7 @@ public class EmbedPageBuilder
         return this;
     }
 
-    public EmbedPageBuilder AddButton(string id = "", string text = "", string name = "", string link = "", string color = "000000", string textColor = "ffffff", EmbedItemSize size = EmbedItemSize.Normal, bool center = false, bool inline = false)
+    public EmbedPageBuilder AddButton(string id = "", string text = "", string name = "", string link = "", string color = "000000", string textColor = "eeeeee", EmbedItemSize size = EmbedItemSize.Normal, bool center = false, bool inline = false)
     {
         EmbedItem item = new()
         {
@@ -59,13 +59,25 @@ public class EmbedBuilder
     public List<EmbedItem> Items = new();
     public List<List<EmbedItem>> Pages = new();
 
+    public string? Title { get; set; }
+    public string? Footer { get; set; }
+
+    /// <summary>
+    /// The color (hex) of this embed item's text
+    /// </summary>
+    public string TitleColor = "eeeeee";
+
+    /// <summary>
+    /// The color (hex) of this embed item's text
+    /// </summary>
+    public string FooterColor = "eeeeee";
+
     public EmbedBuilder()
     {
     }
 
     public Embed Generate()
     {
-
         EmbedItem[][] pages = new EmbedItem[Pages.Count][];
 
         for (int i = 0; i < Pages.Count; i++)
@@ -75,7 +87,11 @@ public class EmbedBuilder
 
         return new Embed()
         {
-            Pages = pages
+            Pages = pages,
+            Title = Title,
+            TitleColor = TitleColor,
+            Footer = Footer,
+            FooterColor = FooterColor
         };
     }
 
@@ -85,7 +101,7 @@ public class EmbedBuilder
         return this;
     }
 
-    public EmbedBuilder AddText(string name = "", string text = "", bool inline = false, string textColor = "ffffff")
+    public EmbedBuilder AddText(string name = "", string text = "", bool inline = false, string textColor = "eeeeee")
     {
         EmbedItem item = new()
         {
@@ -99,7 +115,7 @@ public class EmbedBuilder
         return this;
     }
 
-    public EmbedBuilder AddButton(string id = "", string text = "", string name = "", string link = "", string color = "000000", string textColor = "ffffff", EmbedItemSize size = EmbedItemSize.Normal, bool center = false, bool inline = false)
+    public EmbedBuilder AddButton(string id = "", string text = "", string name = "", string link = "", string color = "000000", string textColor = "eeeeee", EmbedItemSize size = EmbedItemSize.Normal, bool center = false, bool inline = false)
     {
         EmbedItem item = new()
         {
