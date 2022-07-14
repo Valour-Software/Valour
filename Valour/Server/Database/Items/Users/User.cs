f6aae872135d7608c09d0af2a41df3e13c00d465
Kobe Albright <kobe.albright@gmail.com>
2022-07-14 13:18:56 -0500

    Inject db for request

--- Valour/Server/Database/Items/Users/User.cs
+++ Valour/Server/Database/Items/Users/User.cs
@@ -253,7 +253,7 @@ public class User : Item, ISharedUser
         return Results.Json(token);
     }
 
-    [ValourRoute(HttpVerbs.Post, "/self/recovery")]
+    [ValourRoute(HttpVerbs.Post, "/self/recovery"), InjectDb]
     public static async Task<IResult> RecoverPasswordRouteAsync(HttpContext ctx, [FromBody] PasswordRecoveryRequest request,
         ILogger<User> logger)
     {
