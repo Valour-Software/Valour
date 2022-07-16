234b0b88d7fd44620b3d69f745adfe272192e547
Coca <coca16622@gmail.com>
2022-07-17 00:54:27 +0100

    Improve FAQ markdown to html

--- Valour/Web/Program.cs
+++ Valour/Web/Program.cs
@@ -24,6 +24,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-Valour.Web.MarkdownToHtml.LoadMarkdown();
+await Valour.Web.MarkdownToHtml.LoadMarkdown();
 app.Run();
 
