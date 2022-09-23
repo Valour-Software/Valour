c927a11d05d9e15c4e0afe0cbd18e93117836147
Kobe Albright <kobe.albright@gmail.com>
2022-09-23 14:50:07 -0500

    Add definitions for friends (we wouldn't know what those are)

---
+++ Valour/Server/Database/Items/Users/UserFriend.cs
@@ -0,0 +1,47 @@
+﻿using Valour.Shared.Items.Users;
+
+namespace Valour.Server.Database.Items.Users;
+
+/*  Valour - A free and secure chat client
+ *  Copyright (C) 2021 Vooper Media LLC
+ *  This program is subject to the GNU Affero General Public license
+ *  A copy of the license should be included - if not, see <http://www.gnu.org/licenses/>
+ */
+
+/// <summary>
+/// Friend objects represent a one-way declaration of friendship. To be actual friends, there must be a
+/// UserFriend object going both ways. Like, you know, a real friendship.
+/// 
+/// ... I'll be your friend!
+/// </summary>
+public class UserFriend : ISharedUserFriend
+{
+    /// <summary>
+    /// The user who added the friend
+    /// </summary>
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; }
+
+    /// <summary>
+    /// The user being added as a friend
+    /// (friendzoned)
+    /// </summary>
+    [ForeignKey("FriendId")]
+    public virtual User Friend { get; set; }
+
+    /// <summary>
+    /// The id of this friendship item
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// The id of the user who added the friend
+    /// </summary>
+    public long UserId { get; set; }
+
+    /// <summary>
+    /// The id of the user being added as a friend
+    /// (friendzoned)
+    /// </summary>
+    public long FriendId { get; set; }
+}
