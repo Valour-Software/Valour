0f948986f417c569bec587697af7d28aa4421d3e
Kobe Albright <kobe.albright@gmail.com>
2022-10-27 13:52:01 -0500

    Connect database to website

--- Valour/Web/Controllers/HomeController.cs
+++ Valour/Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Valour.Server.Database;
 using Valour.Web.Models;
 
 namespace Valour.Web.Controllers;
@@ -7,15 +9,31 @@ namespace Valour.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ValourDB _db;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ValourDB db)
     {
         _logger = logger;
+        _db = db;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        HomeModel model;
+        try
+        {
+            model = new HomeModel((await _db.Users.CountAsync()).ToString(), 
+                                  (await _db.Planets.CountAsync()).ToString());
+        }
+        catch (Exception ex)
+        {
+            // If we fail to reach the database we do not want the homepage to break. Instead use nice words
+            // instead of user counts.
+            model = new HomeModel("Awesome", "Extraordinary");
+            _logger.LogError(ex, "Error in homepage");
+        }
+
+        return View(model);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
