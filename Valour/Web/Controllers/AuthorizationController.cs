0164e0088473291cf38bf394eaaeb7a4ebd7b903
Kobe Albright <kobe.albright@gmail.com>
2022-11-19 09:47:51 -0600

    work on site layout and begin working on authorization

--- Valour/Web/Controllers/AuthorizationController.cs
+++ Valour/Web/Controllers/AuthorizationController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Valour.Server.Database;
+using Valour.Server.Database.Items.Authorization;
+using Valour.Web.Models;
 
 namespace Valour.Web.Controllers;
 
@@ -18,21 +22,52 @@ public class AuthorizationController : Controller
         
     }
 
+
+    /// <summary>
+    /// Returns the auth token from the given HttpContext
+    /// </summary>
+    public async Task<AuthToken> GetAuthToken(HttpContext context)
+    {
+        // Check if already logged in
+        context.Request.Cookies.TryGetValue("token", out var cookieToken);
+
+        if (string.IsNullOrWhiteSpace(cookieToken))
+            return null;
+
+        return await _valourDB.AuthTokens.FindAsync(cookieToken);        
+    } 
+
+    
+    [HttpGet("login")]
+    public async Task<IActionResult> Login(string redirectUrl = "/")
+    {
+        var token = await GetAuthToken(HttpContext);
+
+        // If we're already logged in, redirect to the target
+        if (token is not null)
+            return RedirectPermanent(redirectUrl);
+
+        LoginViewModel model = new();
+
+        // Otherwise, show login page
+        return View(model);
+    }
+
+
     /// <summary>
     /// The Authorize route is sent the oauth response type requested, the client id and permission scope,
     /// and displays the page to log in to authenticate the request.
     /// </summary>
 
     [HttpGet("authorize")]
-    public async Task<IActionResult> AuthorizeAsync(HttpContext context, string responseType, ulong clientId, string redirectUrl, ulong scope)
+    public async Task<IActionResult> AuthorizeAsync(string responseType, ulong clientId, string redirectUrl, ulong scope)
     {
-        // Check if already logged in
-        context.Request.Cookies.TryGetValue("Authorization", out var cookie);
-        if (cookie is not null)
-        {
-            
-        }
+        // If the token is not there or is invalid, send to login page with a redirect back to here
+        var token = await GetAuthToken(HttpContext);
+        if (token is null)
+            return RedirectToAction("login", new { redirectUrl = HttpContext.Request.GetEncodedUrl() });
 
+        // Show authorize dialog
 
         return View();
     }
---
+++ Valour/Web/Models/LoginViewModel.cs
@@ -0,0 +1,7 @@
+namespace Valour.Web.Models;
+
+public class LoginViewModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
