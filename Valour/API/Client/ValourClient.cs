a63b8b12942982d91e9bc6850ddb0cccb85ebaec
Kobe Albright <kobe.albright@gmail.com>
2023-04-17 20:43:57 -0500

    Reorder

--- Valour/API/Client/ValourClient.cs
+++ Valour/API/Client/ValourClient.cs
@@ -940,15 +940,15 @@ public static class ValourClient
 
         // Set reference to self user
         Self = response.Data;
+        
+#if (!DEBUG)
+        Http.BaseAddress = new Uri($"https://{Self.NodeName}.nodes.valour.gg");
+#endif
 
         // Now that we have our user, it should have node data we can use to set up our first node
         // Initialize primary node
         PrimaryNode = new Node();
         await PrimaryNode.InitializeAsync(Self.NodeName, _token);
-        
-#if (!DEBUG)
-        Http.BaseAddress = new Uri($"https://{Self.NodeName}.nodes.valour.gg");
-#endif
 
         // Set node to primary node for main http client
         // Http.DefaultRequestHeaders.Add("X-Server-Select", PrimaryNode.Name);
@@ -997,7 +997,7 @@ public static class ValourClient
 
         // Set reference to self user
         Self = response.Data;
-        
+
 #if (!DEBUG)
         Http.BaseAddress = new Uri($"https://{Self.NodeName}.nodes.valour.gg");
 #endif
