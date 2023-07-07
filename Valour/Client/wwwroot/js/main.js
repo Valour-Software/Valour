81f0706b8ec1f05e4cb3be1cf9895223c606cfd8
Kobe Albright <kobe.albright@gmail.com>
2023-07-06 20:34:29 -0500

    FIx input click on mobile

--- Valour/Client/wwwroot/js/main.js
+++ Valour/Client/wwwroot/js/main.js
@@ -237,7 +237,9 @@ function GetChosenColor() {
 var splits = {};
 
 function SplitWindows(containerId) {
-
+    if (!splits)
+        return;
+    
     if (containerId in splits)
         splits[containerId].destroy();
 
