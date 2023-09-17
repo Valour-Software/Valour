ba3c1284c57f42d04764d52ae02141f2216a4041
Kobe Albright <kobe.albright@gmail.com>
2023-09-17 14:20:36 -0500

    Fix planet menu icons

---
+++ Valour/Client/Utility/ModelListProvider.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Valour.Api.Client;
+using Valour.Api.Items;
+using Valour.Api.Nodes;
+
+namespace Valour.Client.Utility;
+
+public class ModelListProvider<T> where T : LiveModel
+{
+    private readonly string _route;
+    private readonly long? _planetId;
+    
+    
+    public ModelListProvider(string route, long? planetId = null)
+    {
+        this._route = route;
+        this._planetId = planetId;
+    }
+    
+    public async ValueTask<ItemsProviderResult<T>> GetItemsAsync(ItemsProviderRequest request)
+    {
+        var node = _planetId is null ? ValourClient.PrimaryNode : await NodeManager.GetNodeForPlanetAsync(_planetId.Value);
+
+        var result = await node.GetJsonAsync<List<T>>($"{_route}?skip={request.StartIndex}&take={request.Count}");
+
+        if (!result.Success || result.Data is null)
+            return new ItemsProviderResult<T>(new List<T>(), 0);
+        
+        return new ItemsProviderResult<T>(result.Data, result.Data.Count);
+    } 
+}
