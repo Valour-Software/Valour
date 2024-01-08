1939ee181d93c2b11780c1179d60a05f4da9fc1a
Kobe Albright <kobe.albright@gmail.com>
2024-01-08 15:39:28 -0600

    Globalize listener & add static event

--- Valour/Client/Components/Utility/KeyboardListener.razor.js
+++ Valour/Client/Components/Utility/KeyboardListener.razor.js
@@ -1,7 +1,14 @@
-export function init() {
-    window.blazorFuncs.addKeyboardListenerEvent = function (dotNetReference) {
-            document.addEventListener('keydown', event => {
-                dotNetReference.invokeMethodAsync('OnKeyDown', event.key);
-            });
-        }
+export function init(dotNetReference) {
+    document.addEventListener('keydown', event => {
+        dotNetReference.invokeMethodAsync('OnKeyDownInteropAsync', {
+            code: event.code,
+            key: event.key,
+            location: event.location,
+            repeat: event.repeat,
+            shiftKey: event.shiftKey,
+            ctrlKey: event.ctrlKey,
+            altKey: event.altKey,
+            metaKey: event.metaKey,
+        });
+    });
 }
