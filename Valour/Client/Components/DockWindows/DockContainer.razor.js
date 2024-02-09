87f5d6c305bc1cdeafc3c51faeaf5e275d5466c5
Kobe Albright <kobe.albright@gmail.com>
2024-02-09 11:16:56 -0600

    Before I break everything

---
+++ Valour/Client/Components/DockWindows/DockContainer.razor.js
@@ -0,0 +1,12 @@
+﻿export function doSplit(el, direction) {
+    Split(el.children,
+        {
+            direction: direction,
+        }
+    );
+}
+
+export function doUnSplit(el) {
+    // Remove all children with the class `gutter`
+    el.querySelectorAll('.gutter').forEach((e) => e.remove());
+}
