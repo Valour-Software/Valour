af8133c47a9dda51301c3ed9a62e42d874b3c8e3
Kobe Albright <kobe.albright@gmail.com>
2024-02-17 20:49:10 -0600

    mapping

---
+++ Valour/Server/Models/Themes/Theme.cs
@@ -0,0 +1,32 @@
+﻿using Valour.Shared.Models.Themes;
+
+namespace Valour.Server.Models.Themes;
+
+public class Theme : ISharedTheme
+{
+    public long Id { get; set; }
+    public long AuthorId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    
+    public string FontColor { get; set; }
+    public string FontAltColor { get; set; }
+    public string LinkColor { get; set; }
+    
+    public string MainColor1 { get; set; }
+    public string MainColor2 { get; set; }
+    public string MainColor3 { get; set; }
+    public string MainColor4 { get; set; }
+    public string MainColor5 { get; set; }
+    
+    public string TintColor { get; set; }
+    
+    public string VibrantPurple { get; set; }
+    public string VibrantBlue { get; set; }
+    public string VibrantCyan { get; set; }
+    
+    public string PastelCyan { get; set; }
+    public string PastelCyanPurple { get; set; }
+    public string PastelPurple { get; set; }
+    public string PastelRed { get; set; }
+}
