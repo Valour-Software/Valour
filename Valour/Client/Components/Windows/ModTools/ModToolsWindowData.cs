609af84d8649da3392d6b3b4b4b61162e722425d
Kobe Albright <kobe.albright@gmail.com>
2024-07-18 13:41:14 -0400

    Integrate TypeScript & add Staff window

---
+++ Valour/Client/Components/Windows/ModTools/ModToolsWindowData.cs
@@ -0,0 +1,9 @@
+using Valour.Sdk.Models;
+
+namespace Valour.Client.Components.Windows.ModTools;
+
+public class ModToolsWindowData
+{
+    public bool Staff { get; set; }
+    public Planet Planet { get; set; }
+}
