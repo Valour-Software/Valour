6c47914a207feb3dfae998d6383ae529a07b6c78
Kobe Albright <kobe.albright@gmail.com>
2024-08-13 08:02:17 -0400

    Create BlockedUserService.cs

---
+++ Valour/Server/Services/BlockedUserService.cs
@@ -0,0 +1,13 @@
+﻿namespace Valour.Server.Services;
+
+public class BlockedUserService
+{
+    private readonly ValourDB _db;
+    private readonly ILogger<BlockedUserService> _logger;
+    
+    public BlockedUserService(ValourDB db, ILogger<BlockedUserService> logger)
+    {
+        _db = db;
+        _logger = logger;
+    }
+}
