95410a11558a5fbab539f77930c211bbaa018c87
Kobe Albright <kobe.albright@gmail.com>
2025-01-16 13:44:45 -0500

    Friend migrate

---
+++ Valour/Database/Migrations/20250116184428_FixFriendId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Valour.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixFriendId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "user_friends",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "user_friends",
+                newName: "Id");
+        }
+    }
+}
