c461a37ecdb3753cfb1cd98a7a2a601c0e48e004
Kobe Albright <kobe.albright@gmail.com>
2025-02-10 11:48:59 -0500

    Clean up tabstrip mess

--- Valour/Client/Components/Sidebar/TabStripItem.cs
+++ Valour/Client/Components/Sidebar/TabStripItem.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components;
+
 namespace Valour.Client.Components.Sidebar;
 
 public class TabStripItem
@@ -5,4 +7,32 @@ public class TabStripItem
     public string Name;
     public string Icon;
     public int Size;
+    
+    public TabStripItem(string name, string icon, int size)
+    {
+        Name = name;
+        Icon = icon;
+        Size = size;
+    }
+    
+    public virtual RenderFragment RenderContent => builder =>
+    {
+    };
+}
+
+public class TabStripItem<T> : TabStripItem where T : ComponentBase 
+{
+    public TabStripItem(string name, string icon, int size)
+        : base(name, icon, size)
+    {
+        Name = name;
+        Icon = icon;
+        Size = size;
+    }
+    
+    public override RenderFragment RenderContent => builder =>
+    {
+        builder.OpenComponent<T>(0);
+        builder.CloseComponent();
+    };
 }
