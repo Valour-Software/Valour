6eb9a7e5ae969f3274d3404a2b266752ab6150d5
Kobe Albright <kobe.albright@gmail.com>
2025-03-22 00:16:42 -0400

    Update service-worker.published.js

--- Valour/Client.Blazor/wwwroot/service-worker.published.js
+++ Valour/Client.Blazor/wwwroot/service-worker.published.js
@@ -9,13 +9,13 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
-const offlineAssetsExclude = [/^service-worker\.js$/];
+const offlineAssetsExclude = [/^service-worker\.js$/, /^bundled\.min\.css$/];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
 
     // Activate the new service worker as soon as the old one is retired.
-    self.skipWaiting();
+    await self.skipWaiting();
 
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
