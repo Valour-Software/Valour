78abea7e4e58f4da983d11a779b0cfbde277fc42
Kobe Albright <kobe.albright@gmail.com>
2025-05-26 13:59:54 -0400

    Add CustomDropdown components

---
+++ Valour/Client/Components/Utility/CustomDropdownContentData.cs
@@ -0,0 +1,9 @@
+namespace Valour.Client.Components.Utility;
+
+public class CustomDropdownContentData
+{
+    public List<object> Items { get; set; } = new();
+    public RenderFragment<object> ItemTemplate { get; set; }
+    public Func<object, string, bool> SearchFunc { get; set; }
+    public Func<object, Task> OnSelect { get; set; }
+}
