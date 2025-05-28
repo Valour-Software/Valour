178a89a8c4492ff4af0e80b716e9fd75d5b42073
Kobe Albright <kobe.albright@gmail.com>
2025-05-28 17:08:00 -0400

    Various fixes to support presigned

--- Valour/Sdk/Models/MessageAttachment.cs
+++ Valour/Sdk/Models/MessageAttachment.cs
@@ -1,4 +1,6 @@
-﻿using Valour.Shared.Models;
+﻿using Valour.Sdk.Client;
+using Valour.Sdk.Nodes;
+using Valour.Shared.Models;
 
 namespace Valour.Sdk.Models;
 
@@ -30,6 +32,43 @@ public class MessageAttachment : ISharedMessageAttachment
     public MessageAttachment(MessageAttachmentType type)
     {
         Type = type;
+    }
+    
+    private string _signedUrl;
+
+    public async ValueTask<string?> GetSignedUrl(ValourClient client, Node node)
+    {
+        if (Local) // If the attachment is local, we don't need to fetch a signed URL
+        {
+            return Location;
+        }
+        
+        var location = Location;
+        
+        // TODO: Make the base url not hard-coded
+        if (!client.BaseAddress.StartsWith("https://app.valour.gg"))
+        {
+            // In dev environments, swap cdn for local server
+            location = location.Replace("https://cdn.valour.gg/", "");
+        }
+        
+        if (_signedUrl is null)
+        {
+            // Fetch url from CDN
+            try
+            {
+                var result =
+                    await node.GetAsync(location + "/signed");
+                
+                _signedUrl = result.Data;
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error fetching signed URL for attachment: " + ex.Message);
+            }
+        }
+        
+        return _signedUrl;
     }
 }
 
