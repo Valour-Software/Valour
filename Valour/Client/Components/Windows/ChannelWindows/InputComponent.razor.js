ae2d3f8698985e686a8c5608169aceed3bae1c04
Kobe Albright <kobe.albright@gmail.com>
2026-05-01 10:25:19 -0400

    Progress

--- Valour/Client/Components/Windows/ChannelWindows/InputComponent.razor.js
+++ Valour/Client/Components/Windows/ChannelWindows/InputComponent.razor.js
@@ -346,7 +346,7 @@ export function init(dotnet, inputEl) {
                         if (e.code === "ArrowUp") {
                             await ctx.dotnet.invokeMethodAsync('OnUpArrowNonMention');
                         }
-                        await ctx.caretMoveHandler();
+                        await this.caretMoveHandler();
                     }
                     break;
                 case "ArrowLeft":
