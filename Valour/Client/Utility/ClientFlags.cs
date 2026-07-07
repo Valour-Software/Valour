1c5137b6efe3cb20e3a9d0a0ce294ed485675f0a
skyjoshua <mcjoshuagaming@outlook.com>
2026-07-07 02:53:58 +0100

    Uhhh user badge to show other users using the client

--- Valour/Client/Utility/ClientFlags.cs
+++ Valour/Client/Utility/ClientFlags.cs
@@ -5,4 +5,5 @@ public static class ClientFlags
     public static bool GhostTyping { get; set; } = false;
     public static bool ShowDeletedMessages { get; set; } = true;
     public static bool ShowEditHistory { get; set; } = true;
+    public static bool ShowModdedBadge { get; set; } = false;
 }
